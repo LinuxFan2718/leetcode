a56831dabee5c3a296e075e044aa96302ea97b80
Dennis Cahillane <dcahillane@mdsol.com>
2019-10-03 19:15:37 -0400

    all test cases pass

--- 10.py
+++ 10.py
@@ -6,21 +6,35 @@ class Solution:
             return True
         if len(p) == 0:
             return False
-        if p[0] in string.ascii_lowercase:
+        
+        if p[0] in string.ascii_lowercase: # we are matching a letter not . or *
             if len(p) > 1 and p[1] == '*': # a*
-                if p[0] != s[0]:
-                    return self.isMatch(s, p[2:])
-                else: # a* a...
-
-
-
+                if p[0] != s[0]: # matched 0 chars from string
+                    return self.isMatch(s, p[2:]) # drop wildcard a* from p and move on
+                else: # a* matched 1+ a's
+                    while len(s) > 0 and s[0] == p[0]:
+                        if self.isMatch(s, p[2:]):
+                            return True
+                        s = s[1:] # drop all identical characters
+                        if len(s) == 0:
+                            return True
+                    if self.isMatch(s, p[2:]): # check one more time with all a's removed from 
+                        return True
+                    return False
+            elif len(s) == 0:
+                return False
             elif p[0] == s[0]:
                 return self.isMatch(s[1:], p[1:])
             else:
                 return False
+
         elif p[0] == '.':
             if len(p) > 1 and p[1] == '*': # .*
-
+                    while len(s) > 0:
+                        if self.isMatch(s, p[2:]):
+                            return True
+                        s = s[1:] # drop all characters
+                    return True
             else:
                 return self.isMatch(s[1:], p[1:])
             
@@ -33,8 +47,12 @@ class Solution:
         
 
 solution = Solution()
-print(solution.isMatch('', '.*') == True)
-print(solution.isMatch('aa', 'a') == False)
-print(solution.isMatch('aa', 'a*') == True)
-print(solution.isMatch('aab', 'c*a*b') == True)
-print(solution.isMatch('mississippi', 'mis*is*p*.') == True)
+print(1, solution.isMatch('', '.*') == True)
+print(2, solution.isMatch('aa', 'a') == False)
+print(3, solution.isMatch('aa', 'aa') == True)
+print(4, solution.isMatch('aa', 'aaa') == False)
+print(5, solution.isMatch('aa', 'a.') == True)
+print(6, solution.isMatch('aa', '.a') == True)
+print(7, solution.isMatch('aa', 'a*') == True)
+print(8, solution.isMatch('aab', 'c*a*b') == True)
+print(9, solution.isMatch('mississippi', 'mis*is*p*.') == False)
