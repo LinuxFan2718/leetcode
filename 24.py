a95215c32d4cdb969d891bd4a7e23bb510001328
Dennis Cahillane <dcahillane@mdsol.com>
2019-10-06 16:26:07 -0400

    working 24

--- 24.py
+++ 24.py
@@ -7,6 +7,24 @@ class ListNode:
 
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
+        if head == None or head.next == None:
+          return head
+        oldHead = head
+        newHead = head.next
+        thirdNode = newHead.next
+        head = newHead
+        head.next = oldHead
+        head.next.next = thirdNode
+
+        current = oldHead
+        while current and current.next and current.next.next:  
+          oldFirst = current.next
+          oldSecond = current.next.next
+          third = oldSecond.next
+          current.next = oldSecond
+          oldSecond.next = oldFirst
+          oldFirst.next = third
+          current = oldFirst
         return head
             
 def printList(l: ListNode) -> None:
@@ -21,4 +39,13 @@ l1.next.next = ListNode(3)
 l1.next.next.next = ListNode(4)
 
 s = Solution()
+printList(l1)
 printList(s.swapPairs(l1))
+
+l2 = ListNode(1)
+l2.next = ListNode(2)
+l2.next.next = ListNode(3)
+l2.next.next.next = ListNode(4)
+l2.next.next.next.next = ListNode(5)
+printList(l2)
+printList(s.swapPairs(l2))
