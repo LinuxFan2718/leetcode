08b0312877db2e4295418497967d3b301286deb1
Dennis Cahillane <dcahillane@mdsol.com>
2019-10-09 10:59:16 -0400

    30 works but too much memory, words! too large

--- 30.py
+++ 30.py
@@ -1,17 +1,38 @@
 from typing import List
+from itertools import permutations
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        return [0]
+      if len(s) == 0 or len(words) == 0:
+        return []
+      substrings = list(map(lambda x: str.join('', x), permutations(words,len(words))))
+      substringLength = len(substrings[0])
+      startingIndices = []
+      for i in range(0, len(s) - substringLength + 1):
+        if s[i:i+substringLength] in substrings:
+          startingIndices.append(i)
+      return startingIndices
 
 solution = Solution()
-s = "barfoothefoobarman",
+s = "barfoothefoobarman"
 words = ["foo","bar"]
 correctAnswer = [0,9]
 actualAnswer = solution.findSubstring(s, words)
 print(correctAnswer == actualAnswer)
 
-s = "wordgoodgoodgoodbestword",
+s = "wordgoodgoodgoodbestword"
 words = ["word","good","best","word"]
 correctAnswer = []
 actualAnswer = solution.findSubstring(s, words)
 print(correctAnswer == actualAnswer)
+
+s = "wordgoodgoodgoodbestword"
+words = ["word","good","best","good"]
+correctAnswer = [8]
+actualAnswer = solution.findSubstring(s, words)
+print(correctAnswer == actualAnswer)
+
+s = "pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel"
+words = ["dhvf","sind","ffsl","yekr","zwzq","kpeo","cila","tfty","modg","ztjg","ybty","heqg","cpwo","gdcj","lnle","sefg","vimw","bxcb"]
+
+actualAnswer = solution.findSubstring(s, words)
+print(actualAnswer)
