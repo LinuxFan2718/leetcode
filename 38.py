9d8c85ad844b0e03058e6bbff733f021d2e626f9
Dennis Cahillane <dennis@denniscahillane.info>
2019-10-26 12:54:40 -0400

    38 skeleton

---
+++ 38.py
@@ -0,0 +1,24 @@
+from typing import List
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        return [1]
+
+s = Solution()
+candidates = [2,3,6,7]
+target = 7
+solution = [
+  [7],
+  [2,2,3]
+]
+computed = s.combinationSum(candidates, target)
+print(computed == solution, computed)
+
+candidates = [2,3,5]
+target = 8
+solution = [
+  [2,2,2,2],
+  [2,3,3],
+  [3,5]
+]
+computed = s.combinationSum(candidates, target)
+print(computed == solution, computed)
