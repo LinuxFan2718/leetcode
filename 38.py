28ff7a161b77a796c2d471c1e37f4858bccd62b8
Dennis Cahillane <dcahillane@mdsol.com>
2019-10-27 09:29:39 -0400

    filenames sorted out

--- 38.py
+++ 38.py
@@ -1,29 +1,33 @@
 from typing import List
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        memo = []
-        return self.combinationSumHelper(candidates, target, memo)
-
-    def combinationSumHelper(self, candidates: List[int], target: int, memo: List[tuple]) -> List[List[int]]:
-        return [1]
-
-
+    def countAndSay(self, n: int) -> str:
+        say = '1'
+        i = 1
+        while i < n:
+          i += 1
+          newSay = ''
+          startRun = 0
+          while startRun < len(say):
+            lenRun = 1
+            while (startRun + lenRun) < len(say) and say[startRun] == say[startRun + lenRun]:
+              lenRun += 1
+            newSay += str(lenRun) + say[startRun]
+            startRun += lenRun
+          say = newSay
+        return say
+        
 s = Solution()
-candidates = [2,3,6,7]
-target = 7
-solution = [
-  [7],
-  [2,2,3]
-]
-computed = s.combinationSum(candidates, target)
-print(computed == solution, computed)
+# input = 1
+# output = "1"
+# answer = s.countAndSay(input)
+# print(answer == output, answer)
 
-candidates = [2,3,5]
-target = 8
-solution = [
-  [2,2,2,2],
-  [2,3,3],
-  [3,5]
-]
-computed = s.combinationSum(candidates, target)
-print(computed == solution, computed)
+# input = 4
+# output = "1211"
+# answer = s.countAndSay(input)
+# print(answer == output, answer)
+
+
+for i in range(30):
+  answer = s.countAndSay(i)
+  print(i, len(answer))
