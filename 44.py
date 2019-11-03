cac7080fdaf82f50faa61985c5847812b64f2802
Dennis Cahillane <dcahillane@mdsol.com>
2019-11-03 14:32:58 -0500

    optimizations not enough

--- 44.py
+++ 44.py
@@ -4,8 +4,13 @@ class Solution:
       if len(p) == 0:
         return len(s) == 0
       if p[0] == "*":
+        if len(s) > 0 and len(p) > 0 and p[-1] not in ['?', '*'] and s[-1] != p[-1]:
+          return False # ugly speed hack to check last chars match if both letters
+        j = 0
+        while j < len(p) and p[j] == "*":
+          j += 1
         for i in range(len(s)+1):
-          if self.isMatch(s[i:], p[1:]):
+          if self.isMatch(s[i:], p[j:]):
             return True
         return False
       elif p[0] == "?":
@@ -23,6 +28,10 @@ class Solution:
 s = Solution()
 examples = [
   {
+    's': "hi",
+    'p': "*?",
+    'output': True
+  },{
     's': "aa",
     'p': "a",
     'output': False
@@ -46,8 +55,17 @@ examples = [
     's': "aaabbbaabaaaaababaabaaabbabbbbbbbbaabababbabbbaaaaba",
     'p': "a*******b",
     'output': False 
+  },{
+    's': "babbbbaabababaabbababaababaabbaabababbaaababbababaaaaaabbabaaaabababbabbababbbaaaababbbabbbbbbbbbbaabbb",
+    'p': "b**bb**a**bba*b**a*bbb**aba***babbb*aa****aabb*bbb***a",
+    'output': False 
+  },{
+    's': "abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb",
+    'p': "**aa*****ba*a*bb**aa*ab****a*aaaaaa***a*aaaa**bbabb*b*b**aaaaaaaaa*a********ba*bbb***a*ba*bb*bb**a*b*bb",
+    'output': None 
   }
 ]
+
 for example in examples:
   computed = s.isMatch(example['s'],example['p'])
   if computed == example['output']:
