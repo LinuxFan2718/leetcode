672e80e736c830ea926d76c6c5b0d210cf4efd7f
Dennis Cahillane <dcahillane@mdsol.com>
2019-11-29 13:06:58 -0600

    60 green and fast

--- 60.py
+++ 60.py
@@ -1,26 +1,19 @@
 from typing import List
+from math import factorial
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
-        if len(nums) < 2:
-          return
-        for i in range(len(nums) - 1, 0, -1): # right to left
-          if nums[i - 1] < nums[i]: # ascending
-            # swap nums[i - 1] with the smallest number larger than it to its right
-            swap = (-1, 10000000000000) # index, value
-            for j in range(len(nums) - 1, i - 1, -1):
-              if nums[i - 1] < nums[j] and nums[j] < swap[1]:
-                swap = (j, nums[j])
-            nums[i - 1], nums[swap[0]] = nums[swap[0]], nums[i - 1]
-            # reverse nums to the right of nums[i - 1]
-            nums[i:] = nums[:i-1:-1]
-            return
-        nums.reverse()
     def getPermutation(self, n: int, k: int) -> str:
-        ans = [1]
-        for i in range(2, n+1):
-          ans.append(i)
-        for _ in range(1, k):
-          self.nextPermutation(ans)
+        sortedNums = list(range(1, n+1))
+        ans = []
+        k -= 1
+
+        while len(sortedNums) > 0:
+          chunkSize = factorial(n-1)
+          chunksToSkip, _ = divmod(k, chunkSize)
+          digitToAdd = sortedNums.pop(chunksToSkip)
+          ans.append(digitToAdd)
+          n -= 1
+          k -= chunksToSkip * chunkSize
+        
         return ''.join([str(x) for x in ans])
 
 s = Solution()
