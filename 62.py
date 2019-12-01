30cdbfd3cf713a698177928d40e13768cd2eeb1d
Dennis Cahillane <dcahillane@mdsol.com>
2019-12-01 17:37:55 -0500

    62 green but not going to work for related 63

--- 62.py
+++ 62.py
@@ -1,36 +1,50 @@
 from typing import List
+from math import factorial
+from functools import reduce
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        paths = [[(0,0)]]
-        while paths[0][-1] != (m-1, n-1):
-            newPaths = []
-            for path in paths:
-                if path[-1][0]+1 < m:
-                    newPaths.append(path + [(path[-1][0]+1, path[-1][1])])
-                if path[-1][1]+1 < n:
-                    newPaths.append(path + [(path[-1][0], path[-1][1]+1)])
-            paths = newPaths
+        if 1 in (m, n):
+            return 1
+        r = min(m,n) - 1
+        n = max(m,n)
+        #  n! / r! / (n-r)!
+        # ans = len(list(combinations_with_replacement(range(large), small)))
+        # (n+r-1)! / r! / (n-1)!
+        numerator = set(range(1, n+r))
+        denominator1 = int(factorial(r))
+        denominator2 = set(range(1, n))
 
-        return len(paths)
-    
+        multiplierNums = numerator - denominator2
+        product = int(reduce((lambda x,y: x*y), list(multiplierNums)))
+        return int(product / denominator1)
+
 s = Solution()
-# m = 3
-# n = 2
-# o = 3
-# ans = s.uniquePaths(m, n)
-# print(ans == o, ans)
+m = 3
+n = 2
+o = 3
+ans = s.uniquePaths(m, n)
+print(ans == o, ans)
 
-# m = 7
-# n = 3
-# o = 28
-# ans = s.uniquePaths(m, n)
-# print(ans == o, ans)
+m = 7
+n = 3
+o = 28
+ans = s.uniquePaths(m, n)
+print(ans == o, ans)
 
-for i in range(1, 11):
-    print(s.uniquePaths(1, i), s.uniquePaths(2, i), s.uniquePaths(3, i), s.uniquePaths(4, i))
+m = 57
+n = 2
+o = 57
+ans = s.uniquePaths(m, n)
+print(ans == o, ans)
 
-# m = 23
-# n = 12
-# o = None
-# ans = s.uniquePaths(m, n)
-# print(ans == o, ans)
+m = 23
+n = 12
+o = 193536720
+ans = s.uniquePaths(m, n)
+print(ans == o, ans)
+
+m = 1
+n = 2
+o = 1
+ans = s.uniquePaths(m, n)
+print(ans == o, ans)
