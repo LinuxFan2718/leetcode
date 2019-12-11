bbef59dbb6ded9bb1412202d036818e70e8ac253
Dennis Cahillane <dcahillane@mdsol.com>
2019-12-10 19:54:26 -0500

    examples

--- 66.py
+++ 66.py
@@ -1,4 +1,16 @@
 from typing import List
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        
+        return digits
+
+s = Solution()
+i = [1,2,3]
+o = [1,2,4]
+ans = s.plusOne(i)
+print(ans == o, ans)
+
+s = Solution()
+i = [4,3,2,1]
+o = [4,3,2,2]
+ans = s.plusOne(i)
+print(ans == o, ans)
