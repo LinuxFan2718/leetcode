b2ee33141be8c34712c8e3f6bcc48a4cd50db0ba
Dennis Cahillane <dcahillane@mdsol.com>
2019-12-15 18:22:31 -0500

    71 green, fast

--- 71.py
+++ 71.py
@@ -1,7 +1,17 @@
 from typing import List
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        return path
+      ans = []
+      for token in path.split('/'):
+        if token in ['', '.']:
+          pass
+        elif token == '..':
+          if len(ans) > 0:
+            ans.pop()
+        else:
+          ans.append(token)
+      return '/' + '/'.join(ans)
+
 
 s = Solution()
 i = "/home/"
@@ -33,3 +43,21 @@ i = "/a//b////c/d//././/.."
 o = "/a/b/c"
 ans = s.simplifyPath(i)
 print(ans == o, ans)
+
+i = "/."
+o = '/'
+ans = s.simplifyPath(i)
+print(ans == o, ans)
+
+i = "/..."
+o = "/..."
+ans = s.simplifyPath(i)
+print(ans == o, ans)
+
+i = "/..hidden"
+o = "/..hidden"
+ans = s.simplifyPath(i)
+print(ans == o, ans)
+
+i = "/home/of/foo/../../bar/../../is/./here/."
+o = "/is/here"
