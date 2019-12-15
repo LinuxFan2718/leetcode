d0bccca1e1999658e419cab0e287c88783a8f380
Dennis Cahillane <dcahillane@mdsol.com>
2019-12-15 15:26:21 -0500

    69 green

--- 69.py
+++ 69.py
@@ -1,10 +1,17 @@
 # https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Binary_numeral_system_(base_2)
 
 from typing import List
+import math
 class Solution:
     def mySqrt(self, x: int) -> int:
-      binaryString = bin(x)
-      binaryLen = len(binaryString) - 2
+      if x == 0:
+        return x
+      candidate1 = int(math.exp(0.5*math.log(x)))
+      candidate2 = candidate1 + 1
+      if candidate2 * candidate2 <= x:
+        return candidate2
+      else:
+        return candidate1
 
 s = Solution()
 i = 4
@@ -16,4 +23,8 @@ i = 8
 o = 2
 ans = s.mySqrt(i)
 print(ans == o)
-        
+
+i = 2147395600
+o = 46340
+ans = s.mySqrt(i)
+print(ans == o, ans)
