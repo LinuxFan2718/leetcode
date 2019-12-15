6a011e010f250f2ad253ccbfae27049e1c55d81d
Dennis Cahillane <dcahillane@mdsol.com>
2019-12-15 01:41:32 -0500

    68 green

--- 68.py
+++ 68.py
@@ -1,7 +1,29 @@
 from typing import List
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        return words
+      ans = []
+      while len(words) > 0:
+        lineWords = []
+        while len(words) > 0 and sum([len(word)+1 for word in lineWords + [words[0]]]) - 1 <= maxWidth:
+          lineWords.append(words.pop(0))
+        if len(words) == 0 or len(lineWords) == 1: # last line or one-word line, left justify
+          formattedLine = ' '.join(lineWords)
+          ans.append(formattedLine + ' ' * (maxWidth - len(formattedLine)))
+        else: # not last line, fully justify
+          numChars = sum([len(word) for word in lineWords])
+          numSpacesTotal = maxWidth - numChars
+          numGroupSpaces = len(lineWords) - 1
+          smallSpacesLength, numExtraSpaces = divmod(numSpacesTotal, numGroupSpaces)
+          thisLine = ''
+          while len(lineWords) > 1:
+            thisSpace = ' ' * smallSpacesLength
+            if numExtraSpaces > 0:
+              thisSpace += ' '
+              numExtraSpaces -= 1
+            thisLine += lineWords.pop(0) + thisSpace
+          thisLine += lineWords.pop(0)
+          ans.append(thisLine)
+      return ans
 
 s = Solution()
 words = ["This", "is", "an", "example", "of", "text", "justification."]
