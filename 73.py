797de36470a23641aed4cef1096fbc0bc5579690
Dennis Cahillane <dennis@denniscahillane.info>
2019-12-16 21:57:02 -0500

    73 green

--- 73.py
+++ 73.py
@@ -4,6 +4,18 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        rows = []
+        cols = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    rows.append(i)
+                    cols.append(j)
+        for row in rows:
+            matrix[row] = [0] * len(matrix[row])
+        for col in cols:
+            for i in range(len(matrix)):
+                matrix[i][col] = 0
 
 s = Solution()
 i = [
