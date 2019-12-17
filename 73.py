4f788fbdf54696eee80fd2c8a86c3d93298977ef
Dennis Cahillane <dennis@denniscahillane.info>
2019-12-16 21:44:39 -0500

    73 skeleton

---
+++ 73.py
@@ -0,0 +1,33 @@
+from typing import List
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+s = Solution()
+i = [
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]
+]
+o = [
+  [1,0,1],
+  [0,0,0],
+  [1,0,1]
+]
+s.setZeroes(i)
+print(i == o)
+
+i = [
+  [0,1,2,0],
+  [3,4,5,2],
+  [1,3,1,5]
+]
+o = [
+  [0,0,0,0],
+  [0,4,5,0],
+  [0,3,1,0]
+]
+s.setZeroes(i)
+print(i == o)
