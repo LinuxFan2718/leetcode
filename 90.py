742dc5934363441c4a258cb7b1f0059f55e542fe
Dennis Cahillane <dcahillane@mdsol.com>
2020-01-01 16:50:50 -0500

    90 green

--- 90.py
+++ 90.py
@@ -1,7 +1,13 @@
 from typing import List
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        ans = []
+        ans = [sorted(nums)]
+        for i in range(len(nums) - 1, -1, -1):
+          for subset in [subset for subset in ans if len(subset) == i + 1]:
+            for j in range(len(subset)):
+              candidateSubset = subset[:j] + subset[j+1:]
+              if candidateSubset not in ans:
+                ans.append(candidateSubset)
         return ans
 
 s = Solution()
@@ -15,4 +21,22 @@ o = [
   []
 ]
 ans = s.subsetsWithDup(i)
-print(o == ans, ans)
+print(sorted(o) == sorted(ans), ans)
+
+i = [1]
+o = [
+  [1],
+  []
+]
+ans = s.subsetsWithDup(i)
+print(sorted(o) == sorted(ans), ans)
+
+i = []
+o = [[]]
+ans = s.subsetsWithDup(i)
+print(sorted(o) == sorted(ans), ans)
+
+i = [4,4,4,1,4]
+o = [[],[1],[1,4],[1,4,4],[1,4,4,4],[1,4,4,4,4],[4],[4,4],[4,4,4],[4,4,4,4]]
+ans = s.subsetsWithDup(i)
+print(sorted(o) == sorted(ans), ans)
