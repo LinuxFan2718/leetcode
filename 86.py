71db75221f2d8e4f69f2e1fc0c31647a2003d814
Dennis Cahillane <dcahillane@mdsol.com>
2019-12-31 22:07:35 -0500

    86 skeleton

---
+++ 86.py
@@ -0,0 +1,53 @@
+from typing import List
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        return head
+
+def compareLists(l1, l2):
+  while True:
+    if not l1 and not l2:
+      return True
+    if l1.val != l2.val:
+      return False
+    l1 = l1.next
+    l2 = l2.next
+    if not l1 and l2:
+      return False
+    if l1 and not l2:
+      return False
+
+def printList(l1):
+  current = l1
+  while current:
+    if current.next:
+      print(current.val, "->", end =" ")
+    else:
+      print(current.val)
+    current = current.next
+
+s = Solution()
+x = 3
+inputHead = ListNode(1)
+values = [4,3,2,5,2]
+current = inputHead
+for value in values:
+  newCurrent = ListNode(value)
+  current.next = newCurrent
+  current = newCurrent
+
+outputHead = ListNode(1)
+values = [2,2,4,3,5]
+current = outputHead
+for value in values:
+  newCurrent = ListNode(value)
+  current.next = newCurrent
+  current = newCurrent
+
+answerHead = s.partition(inputHead, x)
+print(compareLists(outputHead, answerHead))
+printList(answerHead)
